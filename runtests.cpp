cb751a59e88673ebd7fbf7c867aa0716192f0fe6
cnguyen1209 <cnguyen1209@unm.edu>
2019-10-19 16:43:36 -0600

    Test

--- runtests.cpp
+++ runtests.cpp
@@ -21,10 +21,6 @@ void Assert(bool cond, std::string message)
 
 int main()
 {
-	// TODO:
-	// Write *at least* 15 more tests to fully test
-	// your class.
-
 	StringList a;
 	a.push_back("A");
 	a.push_back("B");
@@ -44,82 +40,78 @@ int main()
 	a.pop_back();
 	Assert(a.back() == "A", "back method");
 
-StringList x;
-x.push_back("H");
-x.push_back("e");
-x.push_back("l");
-x.push_back("l");
-x.push_back("o");
-x.push_back(" ");
-x.push_back("D");
-x.push_back("a");
-x.push_back("v");
-x.push_back("i");
-x.push_back("d");
-Assert(x.getSize() == 11, "size");
-x.reverse();
-//x.printstr();
-x.reverse();
-x.unique();
-//x.printstr();
-Assert(x.getSize() == 10, "size");
-x.pop_back();
-Assert(x.back()== "i", "pop back");
-x.pop_front();
-Assert(x.front() == "e", "pop front");
-x.clear();
-Assert(x.empty() == true, "empty");
+	StringList x;
+	x.push_back("H");
+	x.push_back("e");
+	x.push_back("l");
+	x.push_back("l");
+	x.push_back("o");
+	x.push_back(" ");
+	x.push_back("D");
+	x.push_back("a");
+	x.push_back("v");
+	x.push_back("i");
+	x.push_back("d");
+	Assert(x.getSize() == 11, "size");
+	x.printstr();
+	x.reverse();
+	Assert(x.front() == "d", "Test reverse");
+	x.printstr();
+	x.reverse();
+	x.unique();
+	Assert(x.getSize() == 10, "size");
+	x.pop_back();
+	Assert(x.back()== "i", "pop back");
+	x.pop_front();
+	Assert(x.front() == "e", "pop front");
+	x.clear();
+	Assert(x.empty() == true, "clear and empty");
 
-StringList y;
-y.push_back("C");
-y.push_back("h");
-y.push_back("a");
-y.push_back("s");
-y.push_back("e");
+	StringList y;
+	y.push_back("C");
+	y.push_back("h");
+	y.push_back("a");
+	y.push_back("s");
+	y.push_back("e");
 
-StringList z;
-z=y;
-z.reverse();
-z.pop_front();
-Assert(z.front() == "s", "pop front with reverse");
-z.pop_back();
-Assert(z.back() == "h", "pop back");
-//z.printstr();
+	StringList z;
+	z=y;
+	Assert(z.getSize() == y.getSize(), "Test operator =");
+	z.reverse();
+	z.pop_front();
+	Assert(z.front() == "s", "pop front with reverse");
+	z.pop_back();
+	Assert(z.back() == "h", "pop back");
 
+	StringList c;
+	c.push_back("c");
+	c.push_back("c");
+	c.push_back("c");
+	c.push_back("c");
+	c.push_back("c");
+	c.unique();
+	Assert(c.getSize() == 1, "unique with 5 c's");
 
-
-StringList c;
-c.push_back("c");
-c.push_back("c");
-c.push_back("c");
-c.push_back("c");
-c.push_back("c");
-c.unique();
-Assert(c.getSize() == 1, "unique with 5 c's");
-
-
-StringList j;
-j.push_front("J");
-Assert(j.front() == "J", "push front");
-Assert(j.back() == j.front(), "checking to see if front and back == with 1 elm");
-j.push_back("E");
-j.push_back("R");
-j.push_back("R");
-j.push_back("E");
-j.push_back("Y");
-j.push_back(" ");
-j.push_back("G");
-j.push_back("A");
-j.push_back("R");
-j.push_back("C");
-j.push_back("I");
-j.push_back("A");
-Assert(j.getSize() == 13, "size");
-j.reverse();
-Assert(j.back() == "J", "reverse back");
-Assert(j.front() == "A", "reverse front");
-j.unique();
-Assert(j.getSize() == 12, "unique");
+	StringList j;
+	j.push_front("J");
+	Assert(j.front() == "J", "push front");
+	Assert(j.back() == j.front(), "checking to see if front and back == with 1 elm");
+	j.push_back("E");
+	j.push_back("R");
+	j.push_back("R");
+	j.push_back("E");
+	j.push_back("Y");
+	j.push_back(" ");
+	j.push_back("G");
+	j.push_back("A");
+	j.push_back("R");
+	j.push_back("C");
+	j.push_back("I");
+	j.push_back("A");
+	Assert(j.getSize() == 13, "size");
+	j.reverse();
+	Assert(j.back() == "J", "reverse back");
+	Assert(j.front() == "A", "reverse front");
 
 	return 0;
 }
